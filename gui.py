22fa73d2fe2b3528d1db8e65ebae66e06d48e96e
Sanjeevan Rames <thesilverghost2005@gmail.com>
2024-07-26 22:58:03 +0800

    resize and arrange buttons

--- gui.py
+++ gui.py
@@ -39,64 +39,64 @@ class GUI:
     
     def num_screen(self):
         #subject to change later
-        ttk.Label(self.frame,text="test").grid(row=0,column=1)
+        Label(self.frame,text="test",padx=10,pady=10).grid(row=0,column=1)
 #need to work on the button sizes
     def button_1(self):
-        ttk.Button(self.frame,text="1").grid(row=1,column=0)
+        Button(self.frame,text="1", height=3,width=6,padx=10,pady=10).grid(row=1,column=0)
     
     def button_2(self):
-        ttk.Button(self.frame,text="2").grid(row=1,column=1)
+        Button(self.frame,text="2", height=3,width=6,padx=10,pady=10).grid(row=1,column=1)
     
     def button_3(self):
-        ttk.Button(self.frame,text="3").grid(row=1,column=2)
+        Button(self.frame,text="3", height=3,width=6,padx=10,pady=10).grid(row=1,column=2)
     
     def button_4(self):
-        ttk.Button(self.frame,text="4").grid(row=2,column=0)
+        Button(self.frame,text="4", height=3,width=6,padx=10,pady=10).grid(row=2,column=0)
     
     def button_5(self):
-        ttk.Button(self.frame,text="5").grid(row=2,column=1)
+        Button(self.frame,text="5", height=3,width=6,padx=10,pady=10).grid(row=2,column=1)
     
     def button_6(self):
-        ttk.Button(self.frame,text="6").grid(row=2,column=2)
+        Button(self.frame,text="6", height=3,width=6,padx=10,pady=10).grid(row=2,column=2)
     
     def button_7(self):
-        ttk.Button(self.frame,text="7").grid(row=3,column=0)
+        Button(self.frame,text="7", height=3,width=6,padx=10,pady=10).grid(row=3,column=0)
     
     def button_8(self):
-        ttk.Button(self.frame,text="8").grid(row=3,column=1)
+        Button(self.frame,text="8", height=3,width=6,padx=10,pady=10).grid(row=3,column=1)
     
     def button_9(self):
-        ttk.Button(self.frame,text="9").grid(row=3,column=2)
+        Button(self.frame,text="9", height=3,width=6,padx=10,pady=10).grid(row=3,column=2)
     
     def button_0(self):
-        ttk.Button(self.frame,text="0").grid(row=4,column=0)
+        Button(self.frame,text="0", height=3,width=6,padx=10,pady=10).grid(row=4,column=0)
     
     def button_addition(self):
-        ttk.Button(self.frame,text="+").grid(row=2,column=3)
+        Button(self.frame,text="+", height=3,width=6,padx=10,pady=10).grid(row=2,column=3)
     
     def button_subtraction(self):
-        ttk.Button(self.frame,text="-").grid(row=2,column=4)
+        Button(self.frame,text="-", height=3,width=6,padx=10,pady=10).grid(row=2,column=4)
     
     def button_multiplication(self):
-        ttk.Button(self.frame,text="x").grid(row=3,column=3)
+        Button(self.frame,text="x", height=3,width=6,padx=10,pady=10).grid(row=3,column=3)
     
     def button_division(self):
-        ttk.Button(self.frame,text="÷").grid(row=3,column=4)
+        Button(self.frame,text="÷", height=3,width=6,padx=10,pady=10).grid(row=3,column=4)
     
     def button_power(self):
-        ttk.Button(self.frame,text="^").grid(row=4,column=3)
+        Button(self.frame,text="^", height=3,width=6,padx=10,pady=10).grid(row=4,column=3)
     
     def button_left_parenthesis(self):
-        ttk.Button(self.frame,text="(").grid(row=4,column=1)
+        Button(self.frame,text="(", height=3,width=6,padx=10,pady=10).grid(row=4,column=1)
     
     def button_right_parenthesis(self):
-        ttk.Button(self.frame,text=")").grid(row=4,column=2)
+        Button(self.frame,text=")", height=3,width=6,padx=10,pady=10).grid(row=4,column=2)
     
     def button_equal(self):
-        ttk.Button(self.frame,text="=").grid(row=4,column=4)
+        Button(self.frame,text="=", height=3,width=6,padx=10,pady=10).grid(row=4,column=4)
     
     def button_clear(self):
-        ttk.Button(self.frame,text="C").grid(row=1,column=4)
+        Button(self.frame,text="C", height=3,width=6,padx=10,pady=10).grid(row=1,column=4)
     
     def button_decimal(self):
-        ttk.Button(self.frame,text=".").grid(row=1,column=3)
+        Button(self.frame,text=".", height=3,width=6,padx=10,pady=10).grid(row=1,column=3)
