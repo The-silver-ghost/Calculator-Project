131d050ba81b17af19442c4a385c63fa0e42c5d1
Sanjeevan Rames <thesilverghost2005@gmail.com>
2024-07-27 17:10:26 +0800

    add padding

--- gui.py
+++ gui.py
@@ -2,16 +2,19 @@ from tkinter import *
 from tkinter import ttk
 
 class GUI:
-    def __init__(self,root,frame):
+    def __init__(self,root,frame,padding_x,padding_y):
         self.root = root
         self.frame = frame
+        self.padding_x = padding_x
+        self.padding_y = padding_y
     
     def main_screen(self):
         #sets size for screen
         self.root.resizable(False,False)
         self.frame.grid()
+        
 
-#all buttons go here
+        #all buttons go here
         self.num_screen()
         self.button_1()
         self.button_2()
@@ -34,7 +37,7 @@ class GUI:
         self.button_clear()
         self.button_decimal()
 
-#loops the screen until usr exit
+        #loops the screen until usr exit
         self.root.mainloop()
     
     def num_screen(self):
@@ -42,61 +45,61 @@ class GUI:
         Label(self.frame,text="test",padx=10,pady=10).grid(row=0,column=1)
 #need to work on the button sizes
     def button_1(self):
-        Button(self.frame,text="1", height=3,width=6,padx=10,pady=10).grid(row=1,column=0)
+        Button(self.frame,text="1", height=3,width=6,padx=10,pady=10).grid(row=1,column=0,padx=self.padding_x,pady=self.padding_y)
     
     def button_2(self):
-        Button(self.frame,text="2", height=3,width=6,padx=10,pady=10).grid(row=1,column=1)
+        Button(self.frame,text="2", height=3,width=6,padx=10,pady=10).grid(row=1,column=1,padx=self.padding_x,pady=self.padding_y)
     
     def button_3(self):
-        Button(self.frame,text="3", height=3,width=6,padx=10,pady=10).grid(row=1,column=2)
+        Button(self.frame,text="3", height=3,width=6,padx=10,pady=10).grid(row=1,column=2,padx=self.padding_x,pady=self.padding_y)
     
     def button_4(self):
-        Button(self.frame,text="4", height=3,width=6,padx=10,pady=10).grid(row=2,column=0)
+        Button(self.frame,text="4", height=3,width=6,padx=10,pady=10).grid(row=2,column=0,padx=self.padding_x,pady=self.padding_y)
     
     def button_5(self):
-        Button(self.frame,text="5", height=3,width=6,padx=10,pady=10).grid(row=2,column=1)
+        Button(self.frame,text="5", height=3,width=6,padx=10,pady=10).grid(row=2,column=1,padx=self.padding_x,pady=self.padding_y)
     
     def button_6(self):
-        Button(self.frame,text="6", height=3,width=6,padx=10,pady=10).grid(row=2,column=2)
+        Button(self.frame,text="6", height=3,width=6,padx=10,pady=10).grid(row=2,column=2,padx=self.padding_x,pady=self.padding_y)
     
     def button_7(self):
-        Button(self.frame,text="7", height=3,width=6,padx=10,pady=10).grid(row=3,column=0)
+        Button(self.frame,text="7", height=3,width=6,padx=10,pady=10).grid(row=3,column=0,padx=self.padding_x,pady=self.padding_y)
     
     def button_8(self):
-        Button(self.frame,text="8", height=3,width=6,padx=10,pady=10).grid(row=3,column=1)
+        Button(self.frame,text="8", height=3,width=6,padx=10,pady=10).grid(row=3,column=1,padx=self.padding_x,pady=self.padding_y)
     
     def button_9(self):
-        Button(self.frame,text="9", height=3,width=6,padx=10,pady=10).grid(row=3,column=2)
+        Button(self.frame,text="9", height=3,width=6,padx=10,pady=10).grid(row=3,column=2,padx=self.padding_x,pady=self.padding_y)
     
     def button_0(self):
-        Button(self.frame,text="0", height=3,width=6,padx=10,pady=10).grid(row=4,column=0)
+        Button(self.frame,text="0", height=3,width=6,padx=10,pady=10).grid(row=4,column=0,padx=self.padding_x,pady=self.padding_y)
     
     def button_addition(self):
-        Button(self.frame,text="+", height=3,width=6,padx=10,pady=10).grid(row=2,column=3)
+        Button(self.frame,text="+", height=3,width=6,padx=10,pady=10).grid(row=2,column=3,padx=self.padding_x,pady=self.padding_y)
     
     def button_subtraction(self):
-        Button(self.frame,text="-", height=3,width=6,padx=10,pady=10).grid(row=2,column=4)
+        Button(self.frame,text="-", height=3,width=6,padx=10,pady=10).grid(row=2,column=4,padx=self.padding_x,pady=self.padding_y)
     
     def button_multiplication(self):
-        Button(self.frame,text="x", height=3,width=6,padx=10,pady=10).grid(row=3,column=3)
+        Button(self.frame,text="x", height=3,width=6,padx=10,pady=10).grid(row=3,column=3,padx=self.padding_x,pady=self.padding_y)
     
     def button_division(self):
-        Button(self.frame,text="÷", height=3,width=6,padx=10,pady=10).grid(row=3,column=4)
+        Button(self.frame,text="÷", height=3,width=6,padx=10,pady=10).grid(row=3,column=4,padx=self.padding_x,pady=self.padding_y)
     
     def button_power(self):
-        Button(self.frame,text="^", height=3,width=6,padx=10,pady=10).grid(row=4,column=3)
+        Button(self.frame,text="^", height=3,width=6,padx=10,pady=10).grid(row=4,column=3,padx=self.padding_x,pady=self.padding_y)
     
     def button_left_parenthesis(self):
-        Button(self.frame,text="(", height=3,width=6,padx=10,pady=10).grid(row=4,column=1)
+        Button(self.frame,text="(", height=3,width=6,padx=10,pady=10).grid(row=4,column=1,padx=self.padding_x,pady=self.padding_y)
     
     def button_right_parenthesis(self):
-        Button(self.frame,text=")", height=3,width=6,padx=10,pady=10).grid(row=4,column=2)
+        Button(self.frame,text=")", height=3,width=6,padx=10,pady=10).grid(row=4,column=2,padx=self.padding_x,pady=self.padding_y)
     
     def button_equal(self):
-        Button(self.frame,text="=", height=3,width=6,padx=10,pady=10).grid(row=4,column=4)
+        Button(self.frame,text="=", height=3,width=6,padx=10,pady=10).grid(row=4,column=4,padx=self.padding_x,pady=self.padding_y)
     
     def button_clear(self):
-        Button(self.frame,text="C", height=3,width=6,padx=10,pady=10).grid(row=1,column=4)
+        Button(self.frame,text="C", height=3,width=6,padx=10,pady=10).grid(row=1,column=4,padx=self.padding_x,pady=self.padding_y)
     
     def button_decimal(self):
-        Button(self.frame,text=".", height=3,width=6,padx=10,pady=10).grid(row=1,column=3)
+        Button(self.frame,text=".", height=3,width=6,padx=10,pady=10).grid(row=1,column=3,padx=self.padding_x,pady=self.padding_y)
--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from tkinter import *
 from tkinter import ttk
 
 main_root = Tk()
-main_frame = ttk.Frame(main_root,height=600,width=600)
+main_frame = ttk.Frame(main_root,height=600,width=600,)
 
-gui.GUI(main_root,main_frame).main_screen()
+gui.GUI(main_root,main_frame,5,5).main_screen()
