5a4e7f34bd79d579d5baa0ba638749c078f9aad7
Sanjeevan Rames <thesilverghost2005@gmail.com>
2024-07-31 13:20:20 +0800

    clear merge issue

--- gui.py
+++ gui.py
@@ -16,43 +16,94 @@ class GUI:
 
         #all buttons go here
         self.num_screen()
-        self.buttons()
-        self.text_from_button()
+
+        self.button_1()
+        self.button_2()
+        self.button_3()
+        self.button_4()
+        self.button_5()
+        self.button_6()
+        self.button_7()
+        self.button_8()
+        self.button_9()
+        self.button_0()
+        self.button_addition()
+        self.button_subtraction()
+        self.button_multiplication()
+        self.button_division()
+        self.button_power()
+        self.button_left_parenthesis()
+        self.button_right_parenthesis()
+        self.button_equal()
+        self.button_clear()
+        self.button_decimal()
+
 
         #loops the screen until usr exit
         self.root.mainloop()
     
     def num_screen(self):
         #subject to change later
-        window = PanedWindow(orient="vertical")
-        question = Label(self.root,text=question_string).grid(row=0,column=0,padx=self.padding_x,pady=self.padding_y,columnspan=6)
-        answer = Label(self.root, text=answer_string).grid(row=1,column=0,padx=self.padding_x,pady=self.padding_y,columnspan=6)
 
-    #need to work on the button sizes
-    def buttons(self):
-        Button(self.root,text="1", height=3,width=6,padx=10,pady=10).grid(row=2,column=0,padx=self.padding_x,pady=self.padding_y)
-        Button(self.root,text="2", height=3,width=6,padx=10,pady=10).grid(row=2,column=1,padx=self.padding_x,pady=self.padding_y)
-        Button(self.root,text="3", height=3,width=6,padx=10,pady=10).grid(row=2,column=2,padx=self.padding_x,pady=self.padding_y)
-        Button(self.root,text="4", height=3,width=6,padx=10,pady=10).grid(row=3,column=0,padx=self.padding_x,pady=self.padding_y)
-        Button(self.root,text="5", height=3,width=6,padx=10,pady=10).grid(row=3,column=1,padx=self.padding_x,pady=self.padding_y)
-        Button(self.root,text="6", height=3,width=6,padx=10,pady=10).grid(row=3,column=2,padx=self.padding_x,pady=self.padding_y)
-        Button(self.root,text="7", height=3,width=6,padx=10,pady=10).grid(row=4,column=0,padx=self.padding_x,pady=self.padding_y)
-        Button(self.root,text="8", height=3,width=6,padx=10,pady=10).grid(row=4,column=1,padx=self.padding_x,pady=self.padding_y)
-        Button(self.root,text="9", height=3,width=6,padx=10,pady=10).grid(row=4,column=2,padx=self.padding_x,pady=self.padding_y)
-        Button(self.root,text="0", height=3,width=6,padx=10,pady=10).grid(row=5,column=0,padx=self.padding_x,pady=self.padding_y)
-        Button(self.root,text="+", height=3,width=6,padx=10,pady=10).grid(row=3,column=3,padx=self.padding_x,pady=self.padding_y)
-        Button(self.root,text="-", height=3,width=6,padx=10,pady=10).grid(row=3,column=4,padx=self.padding_x,pady=self.padding_y)
-        Button(self.root,text="x", height=3,width=6,padx=10,pady=10).grid(row=4,column=3,padx=self.padding_x,pady=self.padding_y)
-        Button(self.root,text="÷", height=3,width=6,padx=10,pady=10).grid(row=4,column=4,padx=self.padding_x,pady=self.padding_y)
-        Button(self.root,text="^", height=3,width=6,padx=10,pady=10).grid(row=5,column=3,padx=self.padding_x,pady=self.padding_y)
-        Button(self.root,text="(", height=3,width=6,padx=10,pady=10).grid(row=5,column=1,padx=self.padding_x,pady=self.padding_y)
-        Button(self.root,text=")", height=3,width=6,padx=10,pady=10).grid(row=5,column=2,padx=self.padding_x,pady=self.padding_y)
-        Button(self.root,text="=", height=3,width=6,padx=10,pady=10).grid(row=5,column=4,padx=self.padding_x,pady=self.padding_y)
-        Button(self.root,text="C", height=3,width=6,padx=10,pady=10).grid(row=2,column=4,padx=self.padding_x,pady=self.padding_y)
-        Button(self.root,text=".", height=3,width=6,padx=10,pady=10).grid(row=2,column=3,padx=self.padding_x,pady=self.padding_y)
+        Label(self.root,text="test",padx=10,pady=10,background="#575A5C").grid(row=0,column=1)
+#need to work on the button sizes
+    def button_1(self):
+        Button(self.root,text="1", height=3,width=6,padx=10,pady=10).grid(row=1,column=0,padx=self.padding_x,pady=self.padding_y)
     
-    def text_from_button(self):
-        return Button.cget("text")
+    def button_2(self):
+        Button(self.root,text="2", height=3,width=6,padx=10,pady=10).grid(row=1,column=1,padx=self.padding_x,pady=self.padding_y)
     
-question_string = ""
-answer_string = ""
+    def button_3(self):
+        Button(self.root,text="3", height=3,width=6,padx=10,pady=10).grid(row=1,column=2,padx=self.padding_x,pady=self.padding_y)
+    
+    def button_4(self):
+        Button(self.root,text="4", height=3,width=6,padx=10,pady=10).grid(row=2,column=0,padx=self.padding_x,pady=self.padding_y)
+    
+    def button_5(self):
+        Button(self.root,text="5", height=3,width=6,padx=10,pady=10).grid(row=2,column=1,padx=self.padding_x,pady=self.padding_y)
+    
+    def button_6(self):
+        Button(self.root,text="6", height=3,width=6,padx=10,pady=10).grid(row=2,column=2,padx=self.padding_x,pady=self.padding_y)
+    
+    def button_7(self):
+        Button(self.root,text="7", height=3,width=6,padx=10,pady=10).grid(row=3,column=0,padx=self.padding_x,pady=self.padding_y)
+    
+    def button_8(self):
+        Button(self.root,text="8", height=3,width=6,padx=10,pady=10).grid(row=3,column=1,padx=self.padding_x,pady=self.padding_y)
+    
+    def button_9(self):
+        Button(self.root,text="9", height=3,width=6,padx=10,pady=10).grid(row=3,column=2,padx=self.padding_x,pady=self.padding_y)
+    
+    def button_0(self):
+        Button(self.root,text="0", height=3,width=6,padx=10,pady=10).grid(row=4,column=0,padx=self.padding_x,pady=self.padding_y)
+    
+    def button_addition(self):
+        Button(self.root,text="+", height=3,width=6,padx=10,pady=10).grid(row=2,column=3,padx=self.padding_x,pady=self.padding_y)
+    
+    def button_subtraction(self):
+        Button(self.root,text="-", height=3,width=6,padx=10,pady=10).grid(row=2,column=4,padx=self.padding_x,pady=self.padding_y)
+    
+    def button_multiplication(self):
+        Button(self.root,text="x", height=3,width=6,padx=10,pady=10).grid(row=3,column=3,padx=self.padding_x,pady=self.padding_y)
+    
+    def button_division(self):
+        Button(self.root,text="÷", height=3,width=6,padx=10,pady=10).grid(row=3,column=4,padx=self.padding_x,pady=self.padding_y)
+    
+    def button_power(self):
+        Button(self.root,text="^", height=3,width=6,padx=10,pady=10).grid(row=4,column=3,padx=self.padding_x,pady=self.padding_y)
+    
+    def button_left_parenthesis(self):
+        Button(self.root,text="(", height=3,width=6,padx=10,pady=10).grid(row=4,column=1,padx=self.padding_x,pady=self.padding_y)
+    
+    def button_right_parenthesis(self):
+        Button(self.root,text=")", height=3,width=6,padx=10,pady=10).grid(row=4,column=2,padx=self.padding_x,pady=self.padding_y)
+    
+    def button_equal(self):
+        Button(self.root,text="=", height=3,width=6,padx=10,pady=10).grid(row=4,column=4,padx=self.padding_x,pady=self.padding_y)
+    
+    def button_clear(self):
+        Button(self.root,text="C", height=3,width=6,padx=10,pady=10).grid(row=1,column=4,padx=self.padding_x,pady=self.padding_y)
+    
+    def button_decimal(self):
+        Button(self.root,text=".", height=3,width=6,padx=10,pady=10).grid(row=1,column=3,padx=self.padding_x,pady=self.padding_y)
+
